97230233ca4e62a4ed600d276f446c0ec605df47
Manuel <86536808+omnimu@users.noreply.github.com>
2026-04-16 16:52:29 -0300

    Fix case sensitivity in AWS profile name

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ ng build --aot --prod --extract-css false
 
 # nano ~/.aws/credentials
 
-aws s3 sync dist s3://psira-portal --delete --profile psira --region=af-south-1
+aws s3 sync dist s3://psira-portal --delete --profile Psira --region=af-south-1
 
 aws cloudfront create-invalidation --distribution-id ELKUDD46VAWKE --paths "/index.html" --profile psira
