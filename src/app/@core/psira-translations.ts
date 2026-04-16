deb51ff46dcc550b943d28ad9cc5a25ddfc55d4b
Manuel <86536808+omnimu@users.noreply.github.com>
2026-04-16 16:51:39 -0300

    Rename class psiraTranslations to PsiraTranslations

--- src/app/@core/psira-translations.ts
+++ src/app/@core/psira-translations.ts
@@ -23,7 +23,7 @@ function transformObjectToPath<T extends object | string>(
 }
 
 @Injectable({ providedIn: 'root' })
-export class psiraTranslations extends GenericClass<typeof en>() {
+export class PsiraTranslations extends GenericClass<typeof en>() {
   constructor() {
     super();
     Object.assign(this, transformObjectToPath('', en));
